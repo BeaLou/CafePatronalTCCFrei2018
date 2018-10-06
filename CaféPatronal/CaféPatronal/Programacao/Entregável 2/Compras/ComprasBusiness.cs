5401a23728b0afc22fc3e02e6b0dbcab821b3a6a
oliveirafloyder <oliveirafloyder@gmail.com>
2018-10-06 13:11:45 -0300

    Pastinhas pra organizar + add Vendas e Compras dto/database/business

---
+++ CaféPatronal/CaféPatronal/Programacao/Entregável 2/Compras/ComprasBusiness.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaféPatronal.Programacao.Entregável_2.Compras
+{
+   public class ComprasBusiness
+    {
+
+    }
+}
