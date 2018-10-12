d177f61c7464496587e10e6ecad3787a6c2ea020
oliveirafloyder <oliveirafloyder@gmail.com>
2018-10-11 22:03:57 -0300

    Compras item dto/database/business

--- CaféPatronal/CaféPatronal/Programacao/Entregável 2/Compras/CompraItemBusiness.cs
+++ CaféPatronal/CaféPatronal/Programacao/Entregável 2/Compras/CompraItemBusiness.cs
@@ -8,5 +8,32 @@ namespace CaféPatronal.Programacao.Entregável_2.Compras
 {
     class CompraItemBusiness
     {
+        CompraItemDatabase db = new CompraItemDatabase();
+
+        public int Salvar(CompraItemDTO compra)
+        {
+            return db.Salvar(compra);
+        }
+
+        public void Alterar(CompraItemDTO compra)
+        {
+
+            db.Alterar(compra);
+        }
+
+        public List<CompraItemDTO> Consultar(string produto)
+        {
+            return db.Consultar(produto);
+        }
+
+        public List<CompraItemDTO> Listar()
+        {
+            return db.Listar();
+        }
+
+        public void Remover(int id)
+        {
+            db.Remover(id);
+        }
     }
 }
--- CaféPatronal/CaféPatronal/Programacao/Entregável 2/Compras/CompraItemDTO.cs
+++ CaféPatronal/CaféPatronal/Programacao/Entregável 2/Compras/CompraItemDTO.cs
@@ -9,5 +9,8 @@ namespace CaféPatronal.Programacao.Entregável_2.Compras
     class CompraItemDTO
     {
         //PROP TABA TABA VAI TA FORTE AQ EIN
+        public int id_compraitem { get; set; }
+        public int id_compra { get; set; }
+        public int id_produto { get; set; }
     }
 }
